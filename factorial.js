8fe581ede7049a7221483e056479d725c722c5f4
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-03 14:30:48 -0400

    added whileFactorial and forFactorial functions.

--- factorial.js
+++ factorial.js
@@ -2,14 +2,55 @@
 //    n! = n × (n – 1) × (n – 2) × … × 1 = n × (n – 1)!
 // EX: 4! = 4 x 3 x 2 x 1 
 
+// For Test
+function factorial(num) {
+    // BASE CASE
+    if(current_num === 1 || current_num === 0) {
+      return 1
+    }
+    let result = current_num * recursiveFactorial(current_num - 1);
+    return result
+}
+
+// Recursion
 function recursiveFactorial(current_num) {
+  // BASE CASE
   if(current_num === 1 || current_num === 0) {
     return 1
   }
-  let result = current_num * factorial(current_num - 1)
+  let result = current_num * recursiveFactorial(current_num - 1);
   return result
 }
 
-console.log(recursiveFactorial(5))
+// While Loop
+function whileFactorial(current_num) {
+  if(current_num === 1 || current_num === 0) {
+    return 1
+  }
+  let result = current_num;
+  while(current_num > 1) {
+    result = result * (current_num - 1)
+    console.log("current result:" + current_num)
+    current_num = current_num - 1
+  }
+  return result
+}
 
-module.exports = factorial;
+// For Loop
+function forFactorial(current_num) {
+  if(current_num === 1 || current_num === 0) {
+    return 1
+  }
+  let result = current_num
+  for(let i = current_num - 1; i > 1; i--) {
+    result = result * i
+    console.log("current i: ", i, "\ncurrent result: ", result)
+  }
+  return result
+}
+
+// console.log(forFactorial(5))
+// console.log(whileFactorial(5))
+// console.log(recursiveFactorial(5))
+
+// module.exports = factorial;
