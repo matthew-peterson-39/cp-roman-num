9149cf9a750262faff465d9517acbe7ec9a74ac0
Matthew Peterson <matthew.s.peterson.39@gmail.com>
2023-10-03 14:03:58 -0400

    added recursiveFactorial function  factorial.js

--- factorial.js
+++ factorial.js
@@ -1,5 +1,15 @@
-function factorial(num) {
-  return 0;
+// Factorial formula: 
+//    n! = n × (n – 1) × (n – 2) × … × 1 = n × (n – 1)!
+// EX: 4! = 4 x 3 x 2 x 1 
+
+function recursiveFactorial(current_num) {
+  if(current_num === 1 || current_num === 0) {
+    return 1
+  }
+  let result = current_num * factorial(current_num - 1)
+  return result
 }
 
-module.exports = factorial;
+console.log(recursiveFactorial(5))
+
+//module.exports = factorial;
